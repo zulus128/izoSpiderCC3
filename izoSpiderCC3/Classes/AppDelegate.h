b63e14eb3d74b8f220cf42d5163d3289025a4f4f
Admin <admin@Admins-Mac.local>
2014-05-04 16:28:19 +0400

    first commit

---
+++ izoSpiderCC3/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  izoSpiderCC3
+//
+//  Created by Admin on 04.05.14.
+//  Copyright vkassin 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ izoSpiderCC3/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  izoSpiderCC3
+//
+//  Created by Admin on 04.05.14.
+//  Copyright vkassin 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
